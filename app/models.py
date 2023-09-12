e528f2c5e7f4b754d78ad9f592d95357bcd59925
TpainMcain <trevorwp77@gmail.com>
2023-09-12 17:10:11 -0500

    add code for Setting up the Flask App, Creating Database Models, User Registration & Authentication, Journaling & Prompts

---
+++ app/models.py
@@ -0,0 +1,18 @@
+# app/models.py
+
+from datetime import datetime
+from app import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    journals = db.relationship('Journal', backref='author', lazy=True)
+
+class Journal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    prompt = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
